b47ac862ba10e2ae76a5dc091d9e94f86231fcb7
Omar Hossam <omar907amor@gmail.com>
2019-02-13 03:22:44 +0200

    Samping tick
    Added the sampling scheme clock counter

--- UART_Receiver.v
+++ UART_Receiver.v
@@ -1,4 +1,11 @@
  module UART_Receiver
+ #(      // FSMD parameters (states names)
+    parameter [1:0]  
+        idle  = 2'b00,
+        start = 2'b01,
+        stop  = 2'b10,
+        transition = 2'b11
+)
  (
     input wire din,
     input wire dnum,snum,
@@ -7,11 +14,30 @@
     input clk,rst,
     
     output wire [7:0] data,
-    output error
+    output wire [2:0] error  //3 bit error (parity, overrun, frame error)
  );
  
+ // signal declration
+wire sample_tick;
+reg qclk_reg;       // Sequential part for a clock counter
+reg qclk_next;      // Compinational part for this counter
  
  
+ 
+ // counter of the sampling tick ( every 16 clk > 1 sample tick )
+always @(posedge clk, posedge rst)
+begin    
+    if(rst)
+        qclk_reg <= 4'b0;
+    else
+        qclk_reg <= qclk_next; 
+end
+
+assign qclk_next = ( ( state == start && qclk_reg == 4'd7 )  ||
+                                    (state != start && qclk_reg == 4'd15) ) ? 4'b0 :  qclk_reg + 4'b1;
+                   
+assign sample_tick = (state == start && qclk == 4'd7 || 
+                             (state != start && qclk_reg == 4'd15) ) ? 1'b1 : 1'b0 ;
  
  
  endmodule
