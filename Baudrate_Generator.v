8c4d37024da9a3a9a882ecae98fa18cf7e35c620
Omar Hossam <omar907amor@gmail.com>
2019-03-03 14:49:10 +0200

    FPGA 50Mhz
    Baudrate is adjusted for the 50MHz FPGAs / Digital clock sends every one second

--- Baudrate_Generator.v
+++ Baudrate_Generator.v
@@ -1,12 +1,4 @@
 module Baudrate_Generator(bd_rate, clk, rst, TX_clk, RX_clk);
-
-    parameter clk_speed = 50e6; // The speed of the clock of your FPGA in Hz (our FPGA is 50 MHz.)
-    
-    // the bit of the counter that is corresponding to the desited baud rate is given by this equation
-    parameter [4:0] Tx_1200 = $floor( $clog2(clk_speed/1200) );     
-    parameter [4:0] Tx_2400 = $floor( $clog2(clk_speed/2400) );
-    parameter [4:0] Tx_4800 = $floor( $clog2(clk_speed/4800) );
-    parameter [4:0] Tx_9600 = $floor( $clog2(clk_speed/9600) );
     
     input wire [1:0] bd_rate;
     input wire clk,rst;
@@ -17,19 +9,21 @@ module Baudrate_Generator(bd_rate, clk, rst, TX_clk, RX_clk);
 // Signal declaration
 reg [19:0] q;
 
-always @(posedge clk,posedge rst) 
-    if(rst) q <= 20'b11_111_111_111_111_111_111;    
+always @(posedge clk,posedge rst)
+begin
+    if(rst) q <= 20'b111_111_111_111_111_111_111_111;    
     else    q <= q - 20'b1;
+end
 
 assign RX_clk = (bd_rate == 2'b00) ? q[19] :        // Baud-rate 1200 RX
                 (bd_rate == 2'b01) ? q[18] :        // Baud-rate 2400 RX
                 (bd_rate == 2'b10) ? q[17] :        // Baud-rate 4800 RX
                 (bd_rate == 2'b11) ? q[16] : 2'bx;  // Baud-rate 9600 RX
 
-assign TX_clk = (bd_rate == 2'b00) ? q[Tx_1200] :        // Baud-rate 1200 TX
-                (bd_rate == 2'b01) ? q[Tx_2400] :        // Baud-rate 2400 TX
-                (bd_rate == 2'b10) ? q[Tx_4800] :        // Baud-rate 4800 TX
-                (bd_rate == 2'b11) ? q[Tx_9600] : 2'bx;  // Baud-rate 9600 TX
+assign TX_clk = (bd_rate == 2'b00) ? q[15] :        // Baud-rate 1200 TX
+                (bd_rate == 2'b01) ? q[14] :        // Baud-rate 2400 TX
+                (bd_rate == 2'b10) ? q[13] :        // Baud-rate 4800 TX
+                (bd_rate == 2'b11) ? q[12] : 2'bx;  // Baud-rate 9600 TX
                                 
 
 endmodule
--- Digital_Clock.v
+++ Digital_Clock.v
@@ -5,17 +5,17 @@ module Digital_Clock  // This module uses the 1 HZ clock of the FPGA
 );
 
 // Segnal declaration
-reg [3:0] q;
+reg [25:0] q;
 
 always @(posedge clk,posedge rst)
 begin
     if(rst)
-        q <= 4'b0;
+        q <= 26'b0;
     else
-        q <= q + 4'b1;
+        q <= q + 26'b1;
 end
 
-assign start_sending = (q == 4'd15) ? 1'b1 : 1'b0; 
+assign start_sending = q[25];  // every 1 second
 
 
 endmodule
